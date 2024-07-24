17ae72edff423ce1ef52377748ccb38ec54a3ce5
陈碧滔 <bitao.chen@qizhidao.com>
2024-07-24 11:05:18 +0800

    feat: 支持vue3 ts

---
+++ src/components/vue-demo/textv3ts.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="red">hello world, this is {{ text }}</div>
+</template>
+
+<script name="text-v3-ts1" setup lang="ts">
+import { ref, toRefs, watch } from "vue";
+
+defineOptions({
+  name: 'text-v3-ts2'
+})
+const props = withDefaults(
+  defineProps<{
+    data?: string;
+  }>(),
+  {
+    data: "john",
+  }
+);
+
+const { data } = toRefs(props);
+const text = ref(data.value);
+watch(data, (value, oldValue) => {
+  if (value !== oldValue) {
+    text.value = value;
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.red {
+  color: red;
+}
+</style>
--- src/plugins/docusaurus-plugin-usevue/usevue.cjs.js
+++ src/plugins/docusaurus-plugin-usevue/usevue.cjs.js
@@ -1,17 +1,1 @@
-"use strict";
-const { VueLoaderPlugin: e } = require("vue-loader");
-module.exports = (s, u) => ({
-  name: "docusaurus-plugin-usevue",
-  configureWebpack: (s, u, r) => ({
-    module: {
-      rules: [
-        { test: /\.vue$/, use: ["vue-loader"] },
-        {
-          test: /\.scss$/,
-          use: ["vue-style-loader", "css-loader", "sass-loader"],
-        },
-      ],
-    },
-    plugins: [new e()],
-  }),
-});
+"use strict";const{VueLoaderPlugin:e}=require("vue-loader");module.exports=(s,u)=>({name:"docusaurus-plugin-usevue",configureWebpack:(s,u,o)=>({module:{rules:[{test:/\.ts$/,loader:"ts-loader",exclude:/node_modules/,options:{appendTsSuffixTo:[/\.vue$/]}},{test:/\.vue$/,loader:"vue-loader"},{test:/\.scss$/,use:["vue-style-loader","css-loader","sass-loader"]}]},plugins:[new e]})});
